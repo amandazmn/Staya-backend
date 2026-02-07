948377eb8654240fc51329c6102c6f5932037238
Amanda Zimmermann <amanda.z1@aluno.ifsc.edu.br>
2026-02-06 23:25:28 -0300

    commit inicial

---
+++ src/main/java/com/staya/api/StayaApplication.java
@@ -0,0 +1,13 @@
+package com.staya.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StayaApplication.class, args);
+	}
+
+}
